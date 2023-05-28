ec44a25a92610232c7b5dcef1f7bba1dd18f8e1d
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-05-28 16:29:49 -0500

    Initial commit of the awsome project

---
+++ src/main/java/com/juanpaabloalvis/projector/ProjectorApplication.java
@@ -0,0 +1,13 @@
+package com.juanpaabloalvis.projector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanpaabloalvis/projector/ProjectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanpaabloalvis.projector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
